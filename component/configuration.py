28dbf0b0682b0e26086308d04e6f97b05fe84af5
bohdan_rudenko <b.r.rudenko@gmail.com>
2025-01-08 17:07:56 +0200

    feature: Replaced console output with pygame-based visualization

---
+++ component/configuration.py
@@ -0,0 +1,9 @@
+# Configuration
+FPS = 10  # Frames per second
+CELL_SIZE = 20  # Size of each cell in pixels
+# Number of cells in the vertical and horizontal directions
+v_cells_number = 25
+h_cells_number = 25
+# Colors for live and dead cells
+live_color = (0, 255, 0)
+dead_color = (0, 0, 0)
